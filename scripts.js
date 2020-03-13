fe65f8f84e4cb4441a2f4071b3b384da38525968
Ram <45368708+ramgallagher@users.noreply.github.com>
2020-03-13 17:40:56 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,35 @@
+/* menu dropdown*/
+
 const button = document.getElementById("btn-menu");
 const list = document.getElementById("list");
 
 list.style.display = "none";
 
-button.addEventListener("click",(event) => {
-    if (list.style.display=="none") {
+button.addEventListener("click", (event) => {
+    if (list.style.display == "none") {
         list.style.display = "block";
     } else {
         list.style.display = "none";
     }
+})
+
+function dropdown() {
+    var menu = document.getElementById('lista');
+    if (menu.style.visibility === "hidden") {
+        menu.style.visibility = "visible";
+    } else {
+        menu.style.visibility = "hidden";
+    }
+
+}
+
+
+const body = document.querySelector('body');
+document.getElementById('dark').addEventListener('click', () => {
+    const body = document.querySelector('body');
+    body.classList.add('dark');
+    var logo = document.getElementById('logo')
+    logo.src = "./img/gifOF_logo_dark.png";
+
+
 })
