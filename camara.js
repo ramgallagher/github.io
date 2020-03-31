81ce37f184baeb24c3034a6e82c951d4b9a9286d
Ram <45368708+ramgallagher@users.noreply.github.com>
2020-03-31 18:50:34 -0300

    Add files via upload

--- camara.js
+++ camara.js
@@ -1,6 +1,11 @@
 const contenedorCrear = document.getElementById('ventana-crear');
 const contenedorCaptura = document.getElementById('ventana-captura');
 const botonComenzar = document.getElementById('btn-comenzar');
+const botonCamara = document.querySelector('.btn-cam');
+const botonCapturar = document.getElementById('btn-capturar');
+const botonRecord = document.querySelector('.btn-record');
+const botonListo = document.querySelector('.btn-listo');
+
 
 
 
@@ -10,7 +15,22 @@ contenedorCaptura.style.display = "none";
 botonComenzar.addEventListener('click', function() {
     contenedorCrear.style.display = "none";
     contenedorCaptura.style.display = "block";
+    botonCapturar.style.display = "block";
+    botonRecord.style.display = "none";
+    botonListo.style.display = "none";
+    
 })
+
+botonCapturar.addEventListener('click', () => {
+    contenedorCrear.style.display = "none";
+    contenedorCaptura.style.display = "block";
+    botonCapturar.style.display = "none";
+    botonCamara.style.display = "none";
+    botonRecord.style.display = "flex";
+    botonListo.style.display = "block";
+    
+} )
+
 
 
 
