d212ba77a43771075928d0aca7bd17d60733a5ce
Ram <45368708+ramgallagher@users.noreply.github.com>
2020-04-01 16:59:57 -0300

    Add files via upload

--- camara.js
+++ camara.js
@@ -5,7 +5,7 @@ const contenedorCrear = document.getElementById('ventana-crear');
 const contenedorCaptura = document.getElementById('ventana-captura');
 const botonComenzar = document.getElementById('btn-comenzar');
 const botonCamara = document.querySelector('.btn-cam');
-const botonCapturar = document.getElementById('btn-capturar');
+const botonCapturar = document.querySelector('.btn-capturar');
 const botonRecord = document.querySelector('.btn-record');
 const botonListo = document.querySelector('.btn-listo');
 const video = document.getElementById('contenedor-captura');
@@ -117,3 +117,4 @@ botonListo.addEventListener('click', ()=> {
     
     
 /* fin cronometro */ 
+
