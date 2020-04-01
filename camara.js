8b7f2ce626c7dcece43c3a7380dae01287bf6893
Ram <45368708+ramgallagher@users.noreply.github.com>
2020-04-01 15:46:06 -0300

    Add files via upload

--- camara.js
+++ camara.js
@@ -1,3 +1,6 @@
+
+/* Declaracion de variables y manejo del DOM */
+
 const contenedorCrear = document.getElementById('ventana-crear');
 const contenedorCaptura = document.getElementById('ventana-captura');
 const botonComenzar = document.getElementById('btn-comenzar');
@@ -5,6 +8,11 @@ const botonCamara = document.querySelector('.btn-cam');
 const botonCapturar = document.getElementById('btn-capturar');
 const botonRecord = document.querySelector('.btn-record');
 const botonListo = document.querySelector('.btn-listo');
+const video = document.getElementById('contenedor-captura');
+const timer = document.querySelector('.timer');
+const titulo= document.getElementById('titulo');
+const botonRepetir = document.getElementById('btn-repetir');
+const botonSubir = document.getElementById('btn-subir');
 
 
 
@@ -18,6 +26,23 @@ botonComenzar.addEventListener('click', function() {
     botonCapturar.style.display = "block";
     botonRecord.style.display = "none";
     botonListo.style.display = "none";
+    timer.style.display = "none";
+    botonRepetir.style.display = "none";
+    botonSubir.style.display = "none";
+
+
+    navigator.mediaDevices
+    .getUserMedia({
+        audio: false,
+        video: { 
+            heigth:{  max: 480
+            }
+        }
+    })
+    .then(function(stream) {
+        video.srcObject = stream;
+      video.play();
+    })
     
 })
 
@@ -28,9 +53,67 @@ botonCapturar.addEventListener('click', () => {
     botonCamara.style.display = "none";
     botonRecord.style.display = "flex";
     botonListo.style.display = "block";
+    timer.style.display = "block";
+    titulo.innerHTML = "Capturando tu Guifo";
+
+
     
 } )
 
+botonListo.addEventListener('click', ()=> {
+    contenedorCrear.style.display = "none";
+    contenedorCaptura.style.display = "block";
+    botonCapturar.style.display = "none";
+    botonCamara.style.display = "none";
+    botonRecord.style.display = "none";
+    botonListo.style.display = "none";
+    timer.style.display = "block";
+    titulo.innerHTML = "Capturando tu Guifo";
+    botonRepetir.style.display = "inline-block";
+    botonSubir.style.display = "inline-block";
+    
+})
 
+    
 
-
+/* cronometro */ 
+    var inicio=0;
+	var timeout=0;
+ 
+	function empezarDetener(elemento)
+	{
+		if(timeout==0)
+		{
+			// start timer
+ 
+			elemento.value="Detener";
+ 
+			// Obtenemos el valor actual
+			inicio=vuelta=new Date().getTime();
+ 
+			// iniciamos el proceso
+			funcionando();
+		}else{
+			// stop 
+ 
+			elemento.value="Empezar";
+			clearTimeout(timeout);
+			timeout=0;
+		}
+	}
+ 
+	function funcionando()
+	{
+		var actual = new Date().getTime();
+ 		var diff=new Date(actual-inicio);
+ 		var result=LeadingZero(diff.getUTCHours())+":"+LeadingZero(diff.getUTCMinutes())+":"+LeadingZero(diff.getUTCSeconds());
+		document.getElementById('crono').innerHTML = result;
+		timeout=setTimeout("funcionando()",1000);
+	}
+ 
+	function LeadingZero(Time) {
+		return (Time < 10) ? "0" + Time : + Time;
+    }
+    
+    
+/* fin cronometro */ 
--- search.js
+++ search.js
@@ -3,8 +3,9 @@ let sugerencia1 = document.getElementById('sugerencia1');
 let sugerencia2 = document.getElementById('sugerencia2');
 let sugerencia3 = document.getElementById('sugerencia3');
 let sugerencia4 = document.getElementById('sugerencia4');
-
-
+let sugerencia5 = document.getElementById('sugerencia5');
+let sugerencia6 = document.getElementById('sugerencia6');
+let sugerencia7 = document.getElementById('sugerencia7');
 /* botones ver mas */
 
 sugerencia1.addEventListener('click' , () => {
@@ -154,8 +155,121 @@ document.getElementById("texto-resultados").placeholder =   "  Resultados para: 
       })
 
 
-
+      
 /* fin botones ver mas */
+
+    /* botones del search */ 
+    sugerencia5.addEventListener('click' , () => {
+      let ventanaTendencias = document.getElementById('ventana-tendencias');
+      let ventanaResultados = document.getElementById('ventana-resultado');
+      
+    document.getElementById("texto-resultados").placeholder =   "  Resultados para: SIMPSONS"
+      
+      ventanaTendencias.style.display = "none";
+      ventanaResultados.style.display = "block";
+      
+        url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&q=`;
+        url += 'simpsons'
+      
+        fetch(url).then(function (response) {
+          return response.json()
+        }).then(function (json) {
+      
+          console.log(json.data[0].images.fixed_width.url)
+          const results = document.getElementById('tendencias_resultados');
+          let resultsHTML = '';
+          json.data.forEach(function (obj) {
+      
+            const url = obj.images.fixed_width.url
+            const width = obj.images.fixed_width.width
+            resultsHTML += `<img src="${url}" width="313" height="313">`
+      
+      
+          })
+          results.innerHTML = resultsHTML;
+      
+        })
+        .catch(err => {
+          console.error(err);
+        });
+      
+      })
+
+      sugerencia6.addEventListener('click' , () => {
+        let ventanaTendencias = document.getElementById('ventana-tendencias');
+        let ventanaResultados = document.getElementById('ventana-resultado');
+        
+      document.getElementById("texto-resultados").placeholder =   "  Resultados para: CATS"
+        
+        ventanaTendencias.style.display = "none";
+        ventanaResultados.style.display = "block";
+        
+          url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&q=`;
+          url += 'cats'
+        
+          fetch(url).then(function (response) {
+            return response.json()
+          }).then(function (json) {
+        
+            console.log(json.data[0].images.fixed_width.url)
+            const results = document.getElementById('tendencias_resultados');
+            let resultsHTML = '';
+            json.data.forEach(function (obj) {
+        
+              const url = obj.images.fixed_width.url
+              const width = obj.images.fixed_width.width
+              resultsHTML += `<img src="${url}" width="313" height="313">`
+        
+        
+            })
+            results.innerHTML = resultsHTML;
+        
+          })
+          .catch(err => {
+            console.error(err);
+          });
+        
+        })
+
+        sugerencia7.addEventListener('click' , () => {
+          let ventanaTendencias = document.getElementById('ventana-tendencias');
+          let ventanaResultados = document.getElementById('ventana-resultado');
+          
+        document.getElementById("texto-resultados").placeholder =   "  Resultados para: FUTURAMA"
+          
+          ventanaTendencias.style.display = "none";
+          ventanaResultados.style.display = "block";
+          
+            url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&q=`;
+            url += 'futurama'
+          
+            fetch(url).then(function (response) {
+              return response.json()
+            }).then(function (json) {
+          
+              console.log(json.data[0].images.fixed_width.url)
+              const results = document.getElementById('tendencias_resultados');
+              let resultsHTML = '';
+              json.data.forEach(function (obj) {
+          
+                const url = obj.images.fixed_width.url
+                const width = obj.images.fixed_width.width
+                resultsHTML += `<img src="${url}" width="313" height="313">`
+          
+          
+              })
+              results.innerHTML = resultsHTML;
+          
+            })
+            .catch(err => {
+              console.error(err);
+            });
+          
+          })
+
+         /* fin botones del search */ 
+
+
 
 document.addEventListener("DOMContentLoaded", init);
 
